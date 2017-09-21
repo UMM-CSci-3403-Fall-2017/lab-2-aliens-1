41f544b0ae68f5935549c6aac5f6c087781bd37f
dragoteon <woest015@morris.umn.edu>
2017-09-21 11:36:10 -0500

    started on with palindrome, but due to lackof noticeable progress, I switched to disemvowel. I have a basic function set up for disemvowel. I just need to correct some of the errors with malloc and some of the functions that I called. Such as, toupper.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
-
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
+	char* output = malloc(sizeof(str));
+	char_length = sizeof(str);
+	for (int i = 0; i < char_length; i++) {
+//This is long and disgusting, consider turning into a helper function
+		if(toupper(str[i]) == "A" || toupper(str[i]) == "E" || toupper(str[i]) == "I" || toupper(str[i]) == "O" || toupper(str[i]) == "U") {
+	        //Do nothing here, If the string at that point is equal to one of the vowels, I dont want it to output the vowel.  
+		}	
+		else
+	       	{ 
+			output[i] = str[i];
+		}
+
+			
+	}
+  return ("The disemvoweled word is" + output);
+  free(output);
   return (char*) "";
 }
