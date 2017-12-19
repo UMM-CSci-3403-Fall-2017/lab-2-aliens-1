4d43fd68ba0305cb8f7b80cc07c20cc27fbaf33f
dragoteon <woest015@morris.umn.edu>
2017-12-18 21:30:22 -0600

    double checked that everything passed, there may be valgirnd leaks, but it returns the correct values. add comments to the palindrome as well.

--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -7,8 +7,11 @@
 char* str_reverse(char* str) {
   int len, i;
   char* result;
+  //get the length of the string
 
   len = strlen(str);
+  //allocate space for the result
+  //loop the the input and return the word in reverse
   result = (char*) calloc(len+1, sizeof(char));
   for (i=0; i<len; ++i) {
     result[i] = str[len-i-1];
@@ -23,22 +26,28 @@ char* palindrome(char* str) {
   int i;
   bool result = true;
   char *answer;
-
+	// reverse the input string
   rev = str_reverse(str);
   i = 0;
+	//loop through the string and the reverse string
+	//check each haracter to see if they match 
+	//if they dont return false else true.
   while (result && str[i]) {
     if (str[i] != rev[i]) {
       result = false;
     }
     ++i;
   } 
+  //free up reverse string
 free(rev);		
   if (result) {
+	  //if the result is a palindrome set answer to yes
     answer = (char*) calloc(4, sizeof(char));
     answer[0] = 'Y';
     answer[1] = 'e';
     answer[2] = 's';
     answer[3] = '\0';
+    //else set answer to no
   } else {
     answer = (char*) calloc(3, sizeof(char));
     answer[0] = 'N';
